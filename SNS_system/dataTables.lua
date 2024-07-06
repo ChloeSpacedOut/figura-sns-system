11b6503df5f414152484923fda1dda559268f6f0
ChloeSpacedOut <chloespacedout@gmail.com>
2024-07-06 14:56:45 +1000

    minor fixes
    - removed example code from the main script
    - fixed error when scripts loaded in the wrong order

--- SNS_system/dataTables.lua
+++ SNS_system/dataTables.lua
@@ -1,50 +1,7 @@
 snsData = {
-  cubeToggle = {
-    ID = 'cubeToggle',
-    visible = true
-    },
-  cubeColor = {
-    ID = 'cubeColor',
-    color = vec(1,0,0)
-  },
-  cubeTexture = {
-    ID = 'cubeTexture',
-    primaryTexture = 'model.smile'
-  },
-  cubeAnim = {
-    ID = 'cubeAnim',
-    spin = false,
-    roll = false
-  }
+
 }
 
 snsMetadata = {
-  cubeToggle = {
-    ID = 'cubeToggle',
-    paths = {'models.model.cube'},
-    snsFunctions = {
-      visible = 'updatePart'
-    }
-  },
-  cubeColor = {
-    ID = 'cubeColor',
-    paths = {'models.model.cube'},
-    snsFunctions = {
-      color = 'updatePart'
-    }
-  },
-  cubeTexture = {
-    ID = 'cubeTexture',
-    paths = {'models.model.cube'},
-    snsFunctions = {
-      primaryTexture = 'updatePart'
-    }
-  },
-  cubeAnim = {
-    ID = 'cubeAnim',
-    snsFunctions = {
-      spin = 'playAnimation',
-      roll = 'playAnimation'
-    }
-  }
+
 }
